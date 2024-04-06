c3d2e4b34ae284a696516b72ef59a99588f7a279
Rudenches <xzdara@yandex.ru>
2024-04-06 21:46:04 +0300

    third module

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,4 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../constants/constants';
+import { AuthorizationStatus } from '../constants/constants';
+
 import MainPage from '../pages/mainPage/mainPage';
+import FavoritesPage from '../pages/favoritesPages/favoritesPage';
+import LoginPage from '../pages/loginPage/loginPage';
+import OfferPage from '../pages/offerPage/offerPage';
+import NotFoundScreen from '../pages/NotFoundPage';
+import PrivateRoute from '../privateRoute/privateRoute';
 
 type AppScreenProps = {
   cardsNumber: number;
@@ -6,7 +15,36 @@ type AppScreenProps = {
 
 function App({cardsNumber}: AppScreenProps): JSX.Element {
   return (
-    <MainPage cardsNumber = {cardsNumber}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage cardsNumber={cardsNumber}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage/>}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
